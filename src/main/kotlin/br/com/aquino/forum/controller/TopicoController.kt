2afdb1205322453b4dae9fe14d5d46acc9a8b672
aquinolu25 <quinnpw@live.com>
2025-08-13 22:42:43 -0300

    Added a method to update a topic

--- src/main/kotlin/br/com/aquino/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/aquino/forum/controller/TopicoController.kt
@@ -1,5 +1,6 @@
 package br.com.aquino.forum.controller
 
+import br.com.aquino.forum.dto.AtualizacaoTopicoForm
 import br.com.aquino.forum.dto.NovoTopicoForm
 import br.com.aquino.forum.dto.TopicoView
 import br.com.aquino.forum.service.TopicoService
@@ -7,6 +8,7 @@ import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,8 +28,13 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-        service.cadastrar(dto)
+    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
+        service.cadastrar(form)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+        service.atualizar(form)
     }
 
 }
--- src/main/kotlin/br/com/aquino/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/aquino/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.aquino.forum.service
 
+import br.com.aquino.forum.dto.AtualizacaoTopicoForm
 import br.com.aquino.forum.dto.NovoTopicoForm
 import br.com.aquino.forum.dto.TopicoView
 import br.com.aquino.forum.mapper.TopicoFormMapper
@@ -32,6 +33,22 @@ class TopicoService(
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter {t ->
+            t.id == form.id
+        }.findFirst().get()
+        topicos = topicos.minus(topico).plus(Topico(
+            id = form.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        ))
     }
 
 }
